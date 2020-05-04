0eaece4693fa6a18f7a57d0aa7c6cc735ab6d653
baakqiu <baakqiu@tencent.com>
2020-05-04 15:07:50 +0800

    [Update] Fix audit & Update deps

--- src_/compile/identifier.ts
+++
@@ -1,7 +0,0 @@
-import * as estree from 'estree'
-import State from '../state'
-import { OP } from '../share/const'
-
-export function Identifier(node: estree.Identifier, state: State) {
-  state.opCodes.push({ op: OP.LOADV, val: state.symbols.get(node.name).pointer })
-}
--- src_/compile/literal.ts
+++
@@ -1,7 +0,0 @@
-import * as estree from 'estree'
-import State from '../state'
-import { OP } from '../share/const'
-
-export function Literal(node: estree.Literal, state: State) {
-  state.opCodes.push({ op: OP.LOADK, val: node.value })
-}
