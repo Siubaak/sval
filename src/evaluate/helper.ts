0e4dcb12d496c54d71347c31ab4f7de6fe054d27
Vincent Chou <vchou@roblox.com>
2025-07-24 10:26:11 -0700

    [fix]: for statements may not initialize

--- src/evaluate/helper.ts
+++ src/evaluate/helper.ts
@@ -1,23 +1,34 @@
-import { RETURN, SUPER, NOCTOR, CLSCTOR, NEWTARGET, SUPERCALL } from '../share/const.ts'
-import { VariableDeclaration, ClassBody, PropertyDefinition } from './declaration.ts'
-import { define, assign, inherits, callSuper } from '../share/util.ts'
-import { runAsync, runAsyncOptions } from '../share/async.ts'
-import { Identifier } from '../evaluate_n/identifier.ts'
-import { BlockStatement } from './statement.ts'
-import Scope from '../scope/index.ts'
-import evaluate from './index.ts'
-import * as acorn from 'acorn'
+import {
+  RETURN,
+  SUPER,
+  NOCTOR,
+  CLSCTOR,
+  NEWTARGET,
+  SUPERCALL,
+} from "../share/const.ts";
+import {
+  VariableDeclaration,
+  ClassBody,
+  PropertyDefinition,
+} from "./declaration.ts";
+import { define, assign, inherits, callSuper } from "../share/util.ts";
+import { runAsync, runAsyncOptions } from "../share/async.ts";
+import { Identifier } from "../evaluate_n/identifier.ts";
+import { BlockStatement } from "./statement.ts";
+import Scope from "../scope/index.ts";
+import evaluate from "./index.ts";
+import * as acorn from "acorn";
 
 import {
   PatternOptions,
   ObjectPattern,
   ArrayPattern,
   RestElement,
-  AssignmentPattern
-} from './pattern.ts'
+  AssignmentPattern,
+} from "./pattern.ts";
 
 export interface hoistOptions {
-  onlyBlock?: boolean
+  onlyBlock?: boolean;
 }
 
 export function* hoist(
@@ -25,275 +36,302 @@ export function* hoist(
   scope: Scope,
   options: hoistOptions = {}
 ) {
-  const { onlyBlock = false } = options
-  const funcDclrList: any[] = []
-  const funcDclrIdxs: number[] = []
+  const { onlyBlock = false } = options;
+  const funcDclrList: any[] = [];
+  const funcDclrIdxs: number[] = [];
   for (let i = 0; i < block.body.length; i++) {
-    const statement = block.body[i]
-    if (statement.type === 'FunctionDeclaration') {
-      funcDclrList.push(statement)
-      funcDclrIdxs.push(i)
+    const statement = block.body[i];
+    if (statement.type === "FunctionDeclaration") {
+      funcDclrList.push(statement);
+      funcDclrIdxs.push(i);
     } else if (
-      statement.type === 'VariableDeclaration'
-      && ['const', 'let'].indexOf(statement.kind) !== -1
+      statement.type === "VariableDeclaration" &&
+      ["const", "let"].indexOf(statement.kind) !== -1
     ) {
-      yield* VariableDeclaration(statement, scope, { hoist: true, onlyBlock: true })
+      yield* VariableDeclaration(statement, scope, {
+        hoist: true,
+        onlyBlock: true,
+      });
     } else if (!onlyBlock) {
-      yield* hoistVarRecursion(statement as acorn.Statement, scope)
+      yield* hoistVarRecursion(statement as acorn.Statement, scope);
     }
   }
   if (funcDclrIdxs.length) {
     for (let i = funcDclrIdxs.length - 1; i > -1; i--) {
-      block.body.splice(funcDclrIdxs[i], 1)
+      block.body.splice(funcDclrIdxs[i], 1);
     }
-    block.body = funcDclrList.concat(block.body)
+    block.body = funcDclrList.concat(block.body);
   }
 }
 
-function* hoistVarRecursion(statement: acorn.Statement, scope: Scope): IterableIterator<any> {
+function* hoistVarRecursion(
+  statement: acorn.Statement,
+  scope: Scope
+): IterableIterator<any> {
   switch (statement.type) {
-    case 'VariableDeclaration':
-      yield* VariableDeclaration(statement, scope, { hoist: true })
-      break
-    case 'ForInStatement':
-    case 'ForOfStatement':
-      if (statement.left.type === 'VariableDeclaration') {
-        yield* VariableDeclaration(statement.left, scope, { hoist: true })
+    case "VariableDeclaration":
+      yield* VariableDeclaration(statement, scope, { hoist: true });
+      break;
+    case "ForInStatement":
+    case "ForOfStatement":
+      if (statement.left.type === "VariableDeclaration") {
+        yield* VariableDeclaration(statement.left, scope, { hoist: true });
       }
-    case 'ForStatement':
-      if (statement.type === 'ForStatement' && statement.init.type === 'VariableDeclaration') {
-        yield* VariableDeclaration(statement.init, scope, { hoist: true })
+    case "ForStatement":
+      if (
+        statement.type === "ForStatement" &&
+        statement.init?.type === "VariableDeclaration"
+      ) {
+        yield* VariableDeclaration(statement.init, scope, { hoist: true });
       }
-    case 'WhileStatement':
-    case 'DoWhileStatement':
-      yield* hoistVarRecursion(statement.body, scope)
-      break
-    case 'IfStatement':
-      yield* hoistVarRecursion(statement.consequent, scope)
+    case "WhileStatement":
+    case "DoWhileStatement":
+      yield* hoistVarRecursion(statement.body, scope);
+      break;
+    case "IfStatement":
+      yield* hoistVarRecursion(statement.consequent, scope);
       if (statement.alternate) {
-        yield* hoistVarRecursion(statement.alternate, scope)
+        yield* hoistVarRecursion(statement.alternate, scope);
       }
-      break
-    case 'BlockStatement':
+      break;
+    case "BlockStatement":
       for (let i = 0; i < statement.body.length; i++) {
-        yield* hoistVarRecursion(statement.body[i], scope)
+        yield* hoistVarRecursion(statement.body[i], scope);
       }
-      break
-    case 'SwitchStatement':
+      break;
+    case "SwitchStatement":
       for (let i = 0; i < statement.cases.length; i++) {
         for (let j = 0; j < statement.cases[i].consequent.length; j++) {
-          yield* hoistVarRecursion(statement.cases[i].consequent[j], scope)
+          yield* hoistVarRecursion(statement.cases[i].consequent[j], scope);
         }
       }
-      break
-    case 'TryStatement': {
-      const tryBlock = statement.block.body
+      break;
+    case "TryStatement": {
+      const tryBlock = statement.block.body;
       for (let i = 0; i < tryBlock.length; i++) {
-        yield* hoistVarRecursion(tryBlock[i], scope)
+        yield* hoistVarRecursion(tryBlock[i], scope);
       }
-      const catchBlock = statement.handler && statement.handler.body.body
+      const catchBlock = statement.handler && statement.handler.body.body;
       if (catchBlock) {
         for (let i = 0; i < catchBlock.length; i++) {
-          yield* hoistVarRecursion(catchBlock[i], scope)
+          yield* hoistVarRecursion(catchBlock[i], scope);
         }
       }
-      const finalBlock = statement.finalizer && statement.finalizer.body
+      const finalBlock = statement.finalizer && statement.finalizer.body;
       if (finalBlock) {
         for (let i = 0; i < finalBlock.length; i++) {
-          yield* hoistVarRecursion(finalBlock[i], scope)
+          yield* hoistVarRecursion(finalBlock[i], scope);
         }
       }
-      break
+      break;
     }
   }
 }
 
-export function* pattern(node: acorn.Pattern, scope: Scope, options: PatternOptions = {}): IterableIterator<any> {
+export function* pattern(
+  node: acorn.Pattern,
+  scope: Scope,
+  options: PatternOptions = {}
+): IterableIterator<any> {
   switch (node.type) {
-    case 'ObjectPattern':
-      return yield* ObjectPattern(node, scope, options)
-    case 'ArrayPattern':
-      return yield* ArrayPattern(node, scope, options)
-    case 'RestElement':
-      return yield* RestElement(node, scope, options)
-    case 'AssignmentPattern':
-      return yield* AssignmentPattern(node, scope, options)
+    case "ObjectPattern":
+      return yield* ObjectPattern(node, scope, options);
+    case "ArrayPattern":
+      return yield* ArrayPattern(node, scope, options);
+    case "RestElement":
+      return yield* RestElement(node, scope, options);
+    case "AssignmentPattern":
+      return yield* AssignmentPattern(node, scope, options);
     default:
-      throw new SyntaxError('Unexpected token')
+      throw new SyntaxError("Unexpected token");
   }
 }
 
 export interface CtorOptions {
-  construct?: (object: any) => Generator | void
-  superClass?: (...args: any[]) => any
+  construct?: (object: any) => Generator | void;
+  superClass?: (...args: any[]) => any;
 }
 
-import { createFunc as createAnotherFunc } from /*<replace by:='../evaluate/helper.ts'>*/'../evaluate_n/helper.ts'/*</replace>*/
+import { createFunc as createAnotherFunc } from /*<replace by:='../evaluate/helper.ts'>*/ "../evaluate_n/helper.ts"; /*</replace>*/
 export function createFunc(
-  node: acorn.FunctionDeclaration | acorn.FunctionExpression | acorn.ArrowFunctionExpression,
+  node:
+    | acorn.FunctionDeclaration
+    | acorn.FunctionExpression
+    | acorn.ArrowFunctionExpression,
   scope: Scope,
   options: CtorOptions = {}
 ): any {
-  if (/*<replace by:=node.generator\s||\snode.async>*/!node.generator && !node.async/*</replace>*/) {
-    return createAnotherFunc(node, scope, options)
+  if (
+    /*<replace by:=node.generator\s||\snode.async>*/ !node.generator &&
+    !node.async /*</replace>*/
+  ) {
+    return createAnotherFunc(node, scope, options);
   }
 
-  const { superClass, construct } = options
-  const params = node.params
+  const { superClass, construct } = options;
+  const params = node.params;
   const tmpFunc = function* (...args: any[]) {
-    const subScope: Scope = new Scope(scope, true)
-    if (node.type !== 'ArrowFunctionExpression') {
-      subScope.let('this', this)
-      subScope.let('arguments', arguments)
-      subScope.const(NEWTARGET, new.target)
+    const subScope: Scope = new Scope(scope, true);
+    if (node.type !== "ArrowFunctionExpression") {
+      subScope.let("this", this);
+      subScope.let("arguments", arguments);
+      subScope.const(NEWTARGET, new.target);
       if (superClass) {
-        subScope.const(SUPER, superClass)
-        if (construct) subScope.let(SUPERCALL, construct)
+        subScope.const(SUPER, superClass);
+        if (construct) subScope.let(SUPERCALL, construct);
       } else if (construct) {
-        yield* construct(this) as Generator
+        yield* construct(this) as Generator;
       }
     }
 
     for (let i = 0; i < params.length; i++) {
-      const param = params[i]
-      if (param.type === 'Identifier') {
-        subScope.var(param.name, args[i])
-      } else if (param.type === 'RestElement') {
-        yield* RestElement(param, subScope, { kind: 'var', feed: args.slice(i) })
+      const param = params[i];
+      if (param.type === "Identifier") {
+        subScope.var(param.name, args[i]);
+      } else if (param.type === "RestElement") {
+        yield* RestElement(param, subScope, {
+          kind: "var",
+          feed: args.slice(i),
+        });
       } else {
-        yield* pattern(param, subScope, { kind: 'var', feed: args[i] })
+        yield* pattern(param, subScope, { kind: "var", feed: args[i] });
       }
     }
 
-    let result: any
-    if (node.body.type === 'BlockStatement') {
-      yield* hoist(node.body, subScope)
+    let result: any;
+    if (node.body.type === "BlockStatement") {
+      yield* hoist(node.body, subScope);
       result = yield* BlockStatement(node.body, subScope, {
         invasived: true,
-        hoisted: true
-      })
+        hoisted: true,
+      });
     } else {
-      result = yield* evaluate(node.body, subScope)
-      if (node.type === 'ArrowFunctionExpression') {
-        RETURN.RES = result
-        result = RETURN
+      result = yield* evaluate(node.body, subScope);
+      if (node.type === "ArrowFunctionExpression") {
+        RETURN.RES = result;
+        result = RETURN;
       }
     }
 
     if (result === RETURN) {
-      return result.RES
+      return result.RES;
     } else if (new.target) {
-      return subScope.find('this').get()
+      return subScope.find("this").get();
     }
-  }
+  };
 
-  let func: any /*<add>*//*= tmpFunc*//*</add>*/
+  let func: any /*<add>*/ /*= tmpFunc*/ /*</add>*/;
   /*<remove>*/
   if (node.async && node.generator) {
     func = function (): AsyncIterator<any> {
-      const iterator = tmpFunc.apply(this, arguments)
-      let last: Promise<any> = Promise.resolve()
-      let hasCatch = false
+      const iterator = tmpFunc.apply(this, arguments);
+      let last: Promise<any> = Promise.resolve();
+      let hasCatch = false;
       const run = (opts: runAsyncOptions) =>
-        last = last
+        (last = last
           .then(() => runAsync(iterator, assign({ fullRet: true }, opts)))
-          .catch(err => {
+          .catch((err) => {
             if (!hasCatch) {
-              hasCatch = true
-              return Promise.reject(err)
+              hasCatch = true;
+              return Promise.reject(err);
             }
-          })
+          }));
       const asyncIterator: AsyncIterator<any> = {
         next: (res?: any) => run({ res }),
         throw: (err?: any) => run({ err }),
-        return: (ret?: any) => run({ ret })
+        return: (ret?: any) => run({ ret }),
+      };
+      if (typeof Symbol === "function") {
+        (asyncIterator as any)[Symbol.iterator] = function () {
+          return this;
+        };
       }
-      if (typeof Symbol === 'function') {
-        (asyncIterator as any)[Symbol.iterator] = function () { return this }
-      }
-      return asyncIterator
-    }
+      return asyncIterator;
+    };
   } else if (node.async) {
-    func = function () { return runAsync(tmpFunc.apply(this, arguments)) }
+    func = function () {
+      return runAsync(tmpFunc.apply(this, arguments));
+    };
   } else {
-    func = tmpFunc
+    func = tmpFunc;
   }
-  define(func, NOCTOR, { value: true })
+  define(func, NOCTOR, { value: true });
   /*</remove>*/
-  /*<add>*//*
+  /*<add>*/ /*
   if (node.type === 'ArrowFunctionExpression') {
     define(func, NOCTOR, { value: true })
   }
-  *//*</add>*/
+  */ /*</add>*/
 
-  define(func, 'name', {
-    value: (node as acorn.FunctionDeclaration).id
-      && (node as acorn.FunctionDeclaration).id.name
-      || '',
-    configurable: true
-  })
-  define(func, 'length', {
+  define(func, "name", {
+    value:
+      ((node as acorn.FunctionDeclaration).id &&
+        (node as acorn.FunctionDeclaration).id.name) ||
+      "",
+    configurable: true,
+  });
+  define(func, "length", {
     value: params.length,
-    configurable: true
-  })
+    configurable: true,
+  });
 
-  const source = node.loc?.source
+  const source = node.loc?.source;
   if (source) {
-    define(func, 'toString', {
+    define(func, "toString", {
       value: () => source.substring(node.start, node.end),
-      configurable: true
-    })
+      configurable: true,
+    });
   }
 
-  return func
+  return func;
 }
 
 export function* createClass(
   node: acorn.ClassDeclaration | acorn.ClassExpression,
-  scope: Scope,
+  scope: Scope
 ) {
-  const superClass = yield* evaluate(node.superClass, scope)
+  const superClass = yield* evaluate(node.superClass, scope);
 
-  const methodBody = node.body.body
+  const methodBody = node.body.body;
   const construct = function* (object: any) {
     for (let i = 0; i < methodBody.length; i++) {
-      const def = methodBody[i]
-      if (def.type === 'PropertyDefinition' && !def.static) {
-        yield* PropertyDefinition(def, scope, { klass: object, superClass })
+      const def = methodBody[i];
+      if (def.type === "PropertyDefinition" && !def.static) {
+        yield* PropertyDefinition(def, scope, { klass: object, superClass });
       }
     }
-  }
+  };
 
   let klass = function* () {
-    const inst = superClass ? callSuper(this, superClass) : this
-    yield* construct(inst)
-    return inst
-  }
+    const inst = superClass ? callSuper(this, superClass) : this;
+    yield* construct(inst);
+    return inst;
+  };
   for (let i = 0; i < methodBody.length; i++) {
-    const method = methodBody[i]
-    if (method.type === 'MethodDefinition' && method.kind === 'constructor') {
-      klass = createFunc(method.value, scope, { superClass, construct })
-      break
+    const method = methodBody[i];
+    if (method.type === "MethodDefinition" && method.kind === "constructor") {
+      klass = createFunc(method.value, scope, { superClass, construct });
+      break;
     }
   }
 
   if (superClass) {
-    inherits(klass, superClass)
+    inherits(klass, superClass);
   }
 
-  yield* ClassBody(node.body, scope, { klass, superClass })
+  yield* ClassBody(node.body, scope, { klass, superClass });
 
-  define(klass, CLSCTOR, { value: true })
-  define(klass, 'name', {
-    value: node.id && node.id.name || '',
-    configurable: true
-  })
+  define(klass, CLSCTOR, { value: true });
+  define(klass, "name", {
+    value: (node.id && node.id.name) || "",
+    configurable: true,
+  });
 
-  return klass
+  return klass;
 }
 
 export interface ForXHandlerOptions {
-  value: any
+  value: any;
 }
 
 export function* ForXHandler(
@@ -301,24 +339,24 @@ export function* ForXHandler(
   scope: Scope,
   options: ForXHandlerOptions
 ) {
-  const { value } = options
-  const left = node.left
+  const { value } = options;
+  const left = node.left;
 
-  const subScope = new Scope(scope)
-  if (left.type === 'VariableDeclaration') {
-    yield* VariableDeclaration(left, subScope, { feed: value })
-  } else if (left.type === 'Identifier') {
-    const variable = yield* Identifier(left, scope, { getVar: true })
-    variable.set(value)
+  const subScope = new Scope(scope);
+  if (left.type === "VariableDeclaration") {
+    yield* VariableDeclaration(left, subScope, { feed: value });
+  } else if (left.type === "Identifier") {
+    const variable = yield* Identifier(left, scope, { getVar: true });
+    variable.set(value);
   } else {
-    yield* pattern(left, scope, { feed: value })
+    yield* pattern(left, scope, { feed: value });
   }
 
-  let result: any
-  if (node.body.type === 'BlockStatement') {
-    result = yield* BlockStatement(node.body, subScope, { invasived: true })
+  let result: any;
+  if (node.body.type === "BlockStatement") {
+    result = yield* BlockStatement(node.body, subScope, { invasived: true });
   } else {
-    result = yield* evaluate(node.body, subScope)
+    result = yield* evaluate(node.body, subScope);
   }
-  return result
+  return result;
 }
